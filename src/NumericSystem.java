c2efe22ebfa79384753bb2a178319a6cb4846266
Diego <diego.esc.cab@gmail.com>
2023-08-27 00:17:00 -0600

    "Finished NumericSystem"

--- src/NumericSystem.java
+++ src/NumericSystem.java
@@ -3,5 +3,17 @@ public class NumericSystem {
         int numeroDecimal = 500;
         System.out.println("numeroDecimal = " + numeroDecimal);
         System.out.println("numeroDecimal +  = " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+
+        int numeroBinario = 0b111110100;
+        System.out.println("numeroBinario = " + numeroBinario);
+
+        System.out.println("numeroOctal = " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
+
+        int numeroOctal = 0764;
+        System.out.println("numeroOctal = " + numeroOctal);
+
+        System.out.println("numeroHexadecimal = " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+        int numeroHex = 0x1f4;
+        System.out.println("numeroHex = " + numeroHex);
     }
 }
